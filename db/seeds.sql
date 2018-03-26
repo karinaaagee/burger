8f1a008f5e8f57b07e686b5e17aa25f8de34ecfa
Karina <karinag1995@yahoo.com>
2018-03-26 09:46:36 -0700

    buttons not working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", false );
+INSERT INTO burgers (burger_name, devoured) VALUES ("double cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon burger",false);
